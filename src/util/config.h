de9213ced2a578502958041159850cb874db0723
August Bernberg <abearnmountain@gmail.com>
2026-03-31 17:12:45 -0400

    configuration file can not set defaults in program, please update config.c whenever adding a new field with a default value and a cfg_key so that config_init can populate the Config struct

--- src/util/config.h
+++ src/util/config.h
@@ -2,13 +2,26 @@
 #define CONFIG_H_
 
 #include "SDL3/SDL_gpu.h"
+#include "src/util/defines.h"
 
 typedef struct {
+	// platform dependent constants
 	SDL_GPUShaderFormat shader_format;	
+
+	// window options
+	u32 fullscreen;
+	u32 height;
+	u32 width;
+
+	// audio options
+	f32 volume;
+	char* audio_shot; // all paths are defined in config for general sounds
 } Config;
 
 extern Config config;
 
 void config_init(void);
+void config_save(void);
 
 #endif
+
